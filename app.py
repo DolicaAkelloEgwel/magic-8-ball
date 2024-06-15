461acddd1cfa606c81511795abc67b55a2a415cc
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-06-16 00:22:19 +0100

    checking for shaking

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 from app import App
 from app_components import clear_background
+import simple_tildagon as st
+from events.input import Buttons, BUTTON_TYPES
+import time
 
 
 class Magic8Ball(App):
     def __init__(self):
-        pass
+        self.button_states = Buttons(self)
 
     def update(self, delta):
-        pass
+        if self.button_states.get(BUTTON_TYPES["CANCEL"]):
+            self.button_states.clear()
+            self.minimise()
 
     def draw(self, ctx):
         clear_background(ctx)
         ctx.text_align = ctx.CENTER
         ctx.text_baseline = ctx.MIDDLE
-        ctx.move_to(0, 0).gray(1).text("Hello, world!")
+        if st.imu.is_shaken():
+            ctx.move_to(0, 0).gray(1).text("Shaken...")
+        else:
+            ctx.move_to(0, 0).gray(1).text("Hello, world!")
+        time.sleep(2)
 
 
 __app_export__ = Magic8Ball
