2d2e76dc699f4fedd595c0c2d2b1ebd0775ec6fb
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-06-16 00:14:55 +0100

    renaming stuff

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from app import App
 from app_components import clear_background
 
-class HelloWorld(App):
-  def __init__(self):
-    pass
 
-  def update(self, delta):
-    pass
+class Magic8Ball(App):
+    def __init__(self):
+        pass
 
-  def draw(self, ctx):
-    clear_background(ctx)
-    ctx.text_align = ctx.CENTER
-    ctx.text_baseline = ctx.MIDDLE
-    ctx.move_to(0, 0).gray(1).text("Hello, world!")
+    def update(self, delta):
+        pass
 
-__app_export__ = HelloWorld
+    def draw(self, ctx):
+        clear_background(ctx)
+        ctx.text_align = ctx.CENTER
+        ctx.text_baseline = ctx.MIDDLE
+        ctx.move_to(0, 0).gray(1).text("Hello, world!")
+
+
+__app_export__ = Magic8Ball
